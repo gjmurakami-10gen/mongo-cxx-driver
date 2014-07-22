e7174e060b0667dc6bb03c065d7edf01876f9096
Gary Murakami <gary.murakami@10gen.com>
2014-07-22 14:06:11 -0400

    Shell proxy basic function - puts, stop, x, x_s

--- src/mongo/dbtests/cluster_framework_test.cpp
+++ src/mongo/dbtests/cluster_framework_test.cpp
@@ -23,6 +23,7 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
 #include <set>
 #include <string>
@@ -34,16 +35,21 @@ using mongo::ConnectionString;
 using mongo::MockRemoteDBServer;
 using mongo::MockReplicaSet;
 
-using std::set;
 using std::string;
 using std::vector;
 using std::ostream;
 
 using namespace std;
 
-bool die(const std::string& msg)
+bool warn(const string& msg)
 {
-    cout << msg << ": " << strerror(errno) << endl;
+    cerr << msg << ": " << strerror(errno) << endl;
+    return 0;
+}
+
+bool die(const string& msg)
+{
+    warn(msg);
     exit(1);
     return 0;
 }
@@ -62,52 +68,55 @@ namespace mongo {
     class Shell {
     public:
         static const uint16_t DefaultPort;
+        static const int Retries;
         static const string Prompt;
+        static const string Bye;
 
         int sock;
         uint16_t port;
+        pid_t pid;
 
         Shell() {
             port = DefaultPort;
+            connect("mongo_shell.log").read();
         }
 
-        void spawn(const std::string& logFileName) {
-            pid_t pid;
+        void spawn(const string& logFileName) {
             if ((pid = fork()) == 0) {
                 int fdLog = open(logFileName.c_str(), O_WRONLY|O_CREAT|O_TRUNC, 0664);
-                fdLog != -1 || die("spawn error on open of log file for mongo shell");
-                dup2(fdLog, 1) != -1 || die("spawn error on dup of log file for stdout");
-                dup2(fdLog, 2) != -1 || die("spawn error on dup of log file for stderr");
+                fdLog != -1 || die("Shell spawn - error on open of log file for mongo shell");
+                dup2(fdLog, 1) != -1 || die("Shell spawn - error on dup of log file for stdout");
+                dup2(fdLog, 2) != -1 || die("Shell spawn - error on dup of log file for stderr");
                 int fdNull = open("/dev/null", O_RDONLY);
-                fdNull != -1 || die("spawn error on open of /dev/null");
-                dup2(fdNull, 0) != -1 || die("spawn error on dup of /dev/null for stdin");
-                setsid() != -1 || die("spawn error on setsid");
-                cout << "child dup'd" << endl;
+                fdNull != -1 || die("Shell spawn - error on open of /dev/null");
+                dup2(fdNull, 0) != -1 || die("Shell spawn - error on dup of /dev/null for stdin");
+                setsid() != -1 || die("Shell spawn - error on setsid");
                 if ((pid = fork()) == 0) {
                     const char *envMongoShell = getenv("MONGO_SHELL");
                     string mongoShell( envMongoShell ? envMongoShell : "../mongo/mongo");
-                    cout << "mongoShell: " << mongoShell << endl;
                     execv(mongoShell.c_str(), (char *const *)spawn_argvp);
                 }
                 exit(0);
             }
         }
 
-        bool connect(const std::string& logFileName) {
-            sock = socket(AF_INET, SOCK_STREAM, 0);
-            sock != -1 || die("socket call error");
-            struct sockaddr_in sock_addr;
-            memset(&sock_addr, 0, sizeof(sock_addr));
-            sock_addr.sin_family = AF_INET;
-            sock_addr.sin_port = htons(port);
-            for (int i = 0; i < 10; i++) {
-                if (::connect(sock, (struct sockaddr *)&sock_addr, sizeof(sock_addr)) == 0)
-                    return true;
+        Shell& connect(const string& logFileName) {
+            for (int i = 0; i < Retries; i++) {
+                sock = socket(AF_INET, SOCK_STREAM, 0);
+                sock != -1 || die("Shell connect - socket call error");
+                struct sockaddr_in serv_addr;
+                memset(&serv_addr, 0, sizeof(serv_addr));
+                serv_addr.sin_family = AF_INET;
+                inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) > 0 || die("inet_pton error");
+                serv_addr.sin_port = htons(port);
+                if (::connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == 0)
+                    return *this;
+                close(sock);
                 spawn(logFileName);
                 sleep(1);
             }
-            die("Error on connect to mongo shell after many retries");
-            return false;
+            die("Shell connect - error on connect to mongo shell");
+            return *this;
         }
 
         string read(string prompt = Prompt) {
@@ -122,29 +131,60 @@ namespace mongo {
                         return s;
                 }
                 else if (count == 0) {
-                    die("count is 0 (EOF)");
+                    warn("Shell read - count is 0 (EOF)");
                 }
                 else if (count == -1) {
-                    die("socket read error");
+                    warn("Shell read - socket read error");
                 }
             }
+        }
+
+        Shell& puts(const string& s) {
+            string t = s;
+            if (*t.rbegin() != '\n')
+                t += '\n';
+            ssize_t ret = write(sock, t.c_str(), t.length());
+            if (ret == -1)
+                warn("Shell puts - write error");
+            return *this;
+        }
+
+        Shell& stop(void) {
+            puts("exit").read(Bye);
+            shutdown(sock, SHUT_RDWR) != -1 || warn("Shell stop - shutdown error");
+            close(sock) != -1 || warn("Shell stop - socket close error");
+            int stat_loc;
+            waitpid(pid, &stat_loc, 0) != -1 || warn("Shell stop - waitpid error");
+            return *this;
+        }
+
+        string x(const string& s, string prompt = Prompt) {
+            return puts(s).read(prompt);
+        }
+
+        string x_s(const string& s, string prompt = Prompt) {
+           string result = x(s);
+           size_t pos = result.rfind(prompt);
+           if (pos != string::npos)
+               result.resize(pos);
+           return result;
         }
     };
 
     const uint16_t mongo::Shell::DefaultPort = 30001;
+    const int mongo::Shell::Retries = 10;
     const string mongo::Shell::Prompt = "> ";
+    const string mongo::Shell::Bye = "bye\n";
 }
 
 namespace mongo_test {
     using namespace mongo;
 
-    TEST(MockReplicaSetTest, SetName) {
-        MockReplicaSet replSet("n", 3);
-        ASSERT_EQUALS("n", replSet.getSetName());
+    TEST(ShellTest, Basic) {
         Shell *shell = new Shell();
-        bool ret = shell->connect("mongo_shell.log");
-        cout << "connect returned: " << ret << endl;
-        string s = shell->read();
-        cout << "read returned: \"" << s << "\"" << endl;
+        string response = shell->x_s("1+2");
+        ASSERT_EQUALS("3\n", response);
+        shell->stop();
+        delete shell;
     }
 }
